b6d22665e36c9c5d5068f2ebf1084c8b495112ac
Berlincordero <114275282+Berlincordero@users.noreply.github.com>
2024-07-07 20:57:04 -0600

    parte de berlin

---
+++ ProyectoProgramacionAvanzada/Controllers/AppDbcontext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using ProyectoProgramacionAvanzada.Models;
+using System.Collections.Generic;
+
+namespace ProyectoProgramacionAvanzada.Controllers
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        public DbSet<Product_Image> ProductImages { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+    }
+}
--- ProyectoProgramacionAvanzada/obj/Debug/net8.0/ProyectoProgramacionAvanzada.AssemblyInfo.cs
+++ ProyectoProgramacionAvanzada/obj/Debug/net8.0/ProyectoProgramacionAvanzada.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoProgramacionAvanzada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd8b091caf668c667982292af4a574c9c8d76f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoProgramacionAvanzada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoProgramacionAvanzada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
